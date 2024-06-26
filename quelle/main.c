4638557397a3f44cbbc12387eed554bf0dddef1a
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-26 19:28:06 +0200

    stampa uno schermo vuoto

--- quelle/main.c
+++ quelle/main.c
@@ -1,5 +1,30 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <SDL2/SDL.h>
 
-int main(int argc, char ** argv){
+typedef struct ci64 ci64;
+struct ci64 {
+  int64_t r;
+  int64_t i;
+};
+
+int main(int argc, const char **argv) {
+
+  uint64_t width = 480 * 2 + 1;
+  uint64_t height = 480;
+  SDL_Window * window;
+  SDL_Renderer * renderer;
+  SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_CreateWindowAndRenderer(width,height,0,&window,&renderer);
+	SDL_SetRenderDrawColor(renderer,0,0,0,255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+  SDL_SetRenderDrawColor(renderer,0,0,0,255);
+	SDL_RenderClear(renderer);
+	SDL_SetRenderDrawColor(renderer,255,255,255,255);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(1024 + 512);
+	SDL_Quit();
+
   return 0;
 }
