c9e14c1000e37398ec78f25c2429ea1dde74847a
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-26 22:27:10 +0200

    stampa della hue

--- quelle/main.c
+++ quelle/main.c
@@ -11,39 +11,59 @@ struct ci64 {
   int64_t i;
 };
 
+#define A 12
+
 void inputcolor(SDL_Renderer *r, int32_t x, int32_t y) {
   double m = sqrt(x * x + y * y) / 256;
   double a = y / (x != 0 ? x : 0.1);
   double tn = atan(a);
-  double tna = tn >= 0 ? tn : tn + (2 * M_PI);
-  double tna12 = tna * 12;
+  double tna = tn >= 0 ? tn : tn + (M_PI);
+  double tna12 = (tna * A/2) / (M_PI);
   double tna12pi = tna12 / (M_PI);
-  int t = tna12pi;
-  fprintf(stderr, 
+  int t = tna12;
+  fprintf(stdout,
           "x = %3d, "
           "y = %3d, "
           "a = %3f, "
           "r = %3f, "
           "t = %3f, "
-          "t12 = %f,"
-          "t12pi = %f,"
-          "t = %d \n", x, y, a, m, tna, tna12, tna12pi, t);
+          "t12 = %f, "
+          "t12pi = %f, "
+          "t = %d \n",
+          x, y, a, m, tna, tna12, tna12pi, t);
 
-  SDL_Color colors[12] = {
+  SDL_Color colors[A] = {
       {.r = 255, .g = 000, .b = 000}, // red
       {.r = 255, .g = 128, .b = 000}, // orange
       {.r = 255, .g = 255, .b = 000}, // yellow
+
       {.r = 128, .g = 255, .b = 000}, // chartette
       {.r = 000, .g = 255, .b = 000}, // green
       {.r = 000, .g = 255, .b = 128}, // spring green
+
+      //{.r = 0, .g = 0, .b = 0}, // padding
+      //{.r = 0, .g = 0, .b = 0}, // padding
+      //{.r = 0, .g = 0, .b = 0}, // padding
+      //{.r = 0, .g = 0, .b = 0}, // padding
+      //{.r = 0, .g = 0, .b = 0}, // padding
+      //{.r = 0, .g = 0, .b = 0}, // padding
+    
+      //{.r = 0, .g = 0, .b = 0}, // padding
+      //{.r = 0, .g = 0, .b = 0}, // padding
+      //{.r = 0, .g = 0, .b = 0}, // padding
+      //{.r = 0, .g = 0, .b = 0}, // padding
+      //{.r = 0, .g = 0, .b = 0}, // padding
+      //{.r = 0, .g = 0, .b = 0}, // padding
+
       {.r = 000, .g = 255, .b = 255}, // cyan
       {.r = 000, .g = 128, .b = 255}, // azure
       {.r = 000, .g = 000, .b = 255}, // blue
+
       {.r = 128, .g = 000, .b = 255}, // violet
       {.r = 255, .g = 000, .b = 255}, // magenta
       {.r = 255, .g = 000, .b = 128}, // rosa
   };
-  SDL_Color c = colors[t];
+  SDL_Color c = colors[t + (y < 0 ? 6 : 0)];
 
   SDL_SetRenderDrawColor(r, 255, 255, 255, 255);
   SDL_SetRenderDrawColor(r, m * c.r, m * c.g, m * c.b, 255);
